8be8f61391027f4184a95abfd73acddda7429103
Alexander Kiel <alexanderkiel@gmx.net>
2015-10-26 12:18:47 +0100

    Update Clojure and Http-kit

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://git.life.uni-leipzig.local/akiel/datomic-monitoring-influxdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [http-kit "2.1.16"]])
+                 [http-kit "2.1.18"]])
